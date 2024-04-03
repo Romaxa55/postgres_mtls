9005a825b5493a9b19ec0aff9590940f49961711
Roman SHamagin <roman.shamagin@7vpn.com>
2024-04-03 14:56:43 +0300

    init

--- generate-certs.sh
+++ generate-certs.sh
@@ -3,17 +3,27 @@
 # Создание директорий для хранения сертификатов
 mkdir -p certs/{ca,server,client}
 
+# Получение внешнего IP адреса
+EXTERNAL_IP=$(curl -s ifconfig.me)
+
+echo "Внешний IP адрес: $EXTERNAL_IP"
+
 # Генерация ключа и самоподписанного сертификата для CA
 openssl req -new -x509 -days 3650 -nodes -out certs/ca/ca.crt -keyout certs/ca/ca.key -subj "/CN=CA/OU=YourOrg/O=YourOrg/C=RU"
 
 # Генерация ключа сервера
 openssl genrsa -out certs/server/server.key 2048
 
-# Создание запроса на подпись сертификата (CSR) для сервера
-openssl req -new -key certs/server/server.key -out certs/server/server.csr -subj "/CN=postgres-server/OU=YourOrg/O=YourOrg/C=RU"
+# Создание запроса на подпись сертификата (CSR) для сервера с учетом внешнего IP
+openssl req -new -key certs/server/server.key -out certs/server/server.csr -subj "/CN=$EXTERNAL_IP/OU=YourOrg/O=YourOrg/C=RU"
 
-# Подписание сертификата сервера с использованием CA
-openssl x509 -req -in certs/server/server.csr -CA certs/ca/ca.crt -CAkey certs/ca/ca.key -CAcreateserial -out certs/server/server.crt -days 3650
+# Создание конфигурационного файла для расширений сертификата
+cat > certs/server/server.ext <<EOF
+subjectAltName = IP:$EXTERNAL_IP
+EOF
+
+# Подписание сертификата сервера с использованием CA и указанием внешнего IP как SAN (Subject Alternative Name)
+openssl x509 -req -in certs/server/server.csr -CA certs/ca/ca.crt -CAkey certs/ca/ca.key -CAcreateserial -out certs/server/server.crt -days 3650 -extfile certs/server/server.ext
 
 # Генерация ключа клиента
 openssl genrsa -out certs/client/client.key 2048
