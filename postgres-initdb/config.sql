de8e2b3b5ac1d05a31d30efde1a3f9fc1f4b793d
Roman SHamagin <roman.shamagin@7vpn.com>
2024-04-05 10:33:56 +0300

    init

--- postgres-initdb/config.sql
+++ postgres-initdb/config.sql
@@ -1,5 +1,5 @@
-ALTER SYSTEM SET ssl_cert_file TO '/var/lib/pgsql/data/server.crt';
-ALTER SYSTEM SET ssl_key_file TO '/var/lib/pgsql/data/server.key';
-ALTER SYSTEM SET ssl_ca_file TO '/var/lib/pgsql/data/root.crt';
+ALTER SYSTEM SET ssl_cert_file TO '/etc/ssl/server.crt';
+ALTER SYSTEM SET ssl_key_file TO '/etc/ssl/server.key';
+ALTER SYSTEM SET ssl_ca_file TO '/etc/ssl/root.crt';
 ALTER SYSTEM SET ssl TO 'ON';
 
